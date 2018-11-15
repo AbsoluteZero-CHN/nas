471e7b080f13e3b206655c1724629326ca4ae35a
caohao1209@gmail.com <caohao1209@gmial.com>
2018-11-15 21:02:23 +0800

    测试代码

---
+++ src/main/java/cn/noload/nas/web/rest/RocketMQComsumerTest.java
@@ -0,0 +1,26 @@
+package cn.noload.nas.web.rest;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+public class RocketMQComsumerTest {
+    public static void main(String[] args) throws MQClientException {
+
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer();
+        consumer.setNamesrvAddr("10.0.0.202:9876;10.0.0.203:9876");
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+        consumer.setConsumerGroup("nas");
+        consumer.subscribe("transaction", "nas");
+        consumer.registerMessageListener((List<MessageExt> list, ConsumeConcurrentlyContext context) -> {
+            System.out.println("success");
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+        });
+        consumer.start();
+    }
+}
